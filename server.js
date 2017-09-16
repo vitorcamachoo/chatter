1823dacec91afa163c5f3405f7caa0069dd04afd
Vitor Camacho <vitor.camacho@exictos.com>
2017-09-16 20:03:50 +0100

    Add sample example

---
+++ server.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require("path");
+const express = require("express");
+
+const PORT = process.env.PORT || 8080;
+const html = fs.readFileSync('dist/index.html')
+const distPath = path.join(__dirname, 'dist');
+
+const app = express();
+
+// Static server path
+app.use(express.static(distPath));
+
+app.get('/', function(req,res){
+  res.sendFile(path.join(distPath, 'index.html'));
+});
+  
+app.listen(PORT, function () {
+  console.log('Server running on: ', PORT);
+});
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const HtmlWebpackPlugin = require('webpack-html-plugin');
+
+const srcPath = path.join(__dirname, '/src');
+const distPath = path.join(__dirname, '/dist');
+
+const watch = process.argv.includes('--watch');
+const production = process.argv.includes('--prod');
+
+module.exports = {
+  watch,
+  cache: !production,
+  devtool: '#cheap-module-eval-source-map',
+  context: srcPath,
+  entry: {
+    app: './index.js',
+  },
+  output: {
+    path: distPath,
+    filename: '[name].bundle.js',
+  },
+  resolve: {
+    modules: ["node_modules"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title:'Chatter App', 
+      filename: 'index.html',
+      inject: true,
+      template: path.join(srcPath, 'index.html'),
+    })]
+};
